436c3fa846d527b76c1338a9c3168156baf0bbb7
Mustafa-dryad <121792015+Mustafa-dryad@users.noreply.github.com>
2023-02-03 22:25:26 +0100

    Create task 4.cpp

---
+++ task 4.cpp
@@ -0,0 +1,22 @@
+#include "snack.h"
+#include <string>
+#include <iostream>
+   
+
+bool Snack::set_expired(bool exp)
+{ 
+   //try 
+}
+
+
+/*
+Soln
+
+ if(!expired()){
+        expr_=exp;
+        return false;
+    }else{
+        expr_=exp;
+        return true;
+    }
+    */
