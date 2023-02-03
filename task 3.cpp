84856d19e0b3bea97ab5e8254d1aa5c92ba1efee
Mustafa-dryad <121792015+Mustafa-dryad@users.noreply.github.com>
2023-02-03 22:24:06 +0100

    Create task 3.cpp

---
+++ task 3.cpp
@@ -0,0 +1,32 @@
+#include "snack.h"
+#include <string>
+#include <iostream>
+   
+int Snack::set_price(float price)
+{
+   //please try code here
+   }
+   
+   
+  /*
+  Solution
+  
+  
+  if(price<0){
+       return -2;
+   }else{
+       if(price<Snack::price()){
+           price_=price;
+           return-1;
+       }else if(price>Snack::price()){
+           price_=price;
+           return 1;
+       }
+       else{
+           price_=price;
+           return 0;
+       }
+           
+       }
+       */
+  
