91e1a5aeba0371153bb92e2a10b462293d6ef139
Mustafa-dryad <121792015+Mustafa-dryad@users.noreply.github.com>
2023-02-03 22:30:38 +0100

    Create Task 6.cpp

---
+++ Task 6.cpp
@@ -0,0 +1,20 @@
+#include "snack.h"
+#include <string>
+
+#include <iostream>
+using namespace std;
+   
+
+int Snack::how_many_for_ten(double &change) {
+   
+}
+
+
+/* Soln
+#include<math.h>
+int Snack::how_many_for_ten(double &change) const{
+     int no_of_snacks=floor(10.0/price());
+     change=10.0-(no_of_snacks*price());
+     return no_of_snacks;
+}
+*/
